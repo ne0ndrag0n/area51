fa8defa9b059e1e3fa272dc823ff76ccea03b326
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-11 16:06:38 -0400

    crap

--- sfml_demo/1/main.cpp
+++ sfml_demo/1/main.cpp
@@ -90,16 +90,15 @@ int main() {
 
   glBindVertexArray( VAO );
 
-  glBindBuffer( GL_ARRAY_BUFFER, VBO );
-  glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+      glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
-  glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat ), ( GLvoid* ) 0 );
-  glEnableVertexAttribArray( 0 );
+      glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat ), ( GLvoid* ) 0 );
+      glEnableVertexAttribArray( 0 );
+    glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
-  glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-  glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
-  glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indicies ), indicies, GL_STATIC_DRAW );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indicies ), indicies, GL_STATIC_DRAW );
 
   glBindVertexArray( 0 );
 
