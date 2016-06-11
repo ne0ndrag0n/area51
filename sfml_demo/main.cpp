38f0ed4a5a9abbb11a507491179b01890384256b
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-11 09:36:54 -0400

    Successful SFML test

--- sfml_demo/main.cpp
+++ sfml_demo/main.cpp
@@ -13,7 +13,7 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
     "out vec4 color;\n"
     "void main()\n"
     "{\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "color = vec4(0.0f, 0.5f, 0.0f, 1.0f);\n"
     "}\n\0";
 
 GLuint compileShaders() {
@@ -71,17 +71,56 @@ int main() {
   // Initialize GLEW to setup the OpenGL Function pointers
   glewInit();
 
-  compileShaders();
+  GLuint program = compileShaders();
+
+  glViewport( 0, 0, 640, 480 );
+
+  GLfloat vertices[] = {
+    0.5f,  0.5f, 0.0f,  // Top Right
+    0.5f, -0.5f, 0.0f,  // Bottom Right
+    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    -0.5f,  0.5f, 0.0f   // Top Left
+  };
+  GLuint indicies[] = { 0, 1, 3, 1, 2, 3 };
+
+  GLuint VBO, VAO, EBO;
+  glGenVertexArrays( 1, &VAO );
+  glGenBuffers( 1, &VBO );
+  glGenBuffers( 1, &EBO );
+
+  glBindVertexArray( VAO );
+
+  glBindBuffer( GL_ARRAY_BUFFER, VBO );
+  glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+  glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat ), ( GLvoid* ) 0 );
+  glEnableVertexAttribArray( 0 );
+
+  glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+  glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+  glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indicies ), indicies, GL_STATIC_DRAW );
+
+  glBindVertexArray( 0 );
 
   while( mainWindow.isOpen() ) {
     sf::Event event;
     while( mainWindow.pollEvent( event ) ) {
       if( event.type == sf::Event::Closed ) {
         mainWindow.close();
+        glDeleteVertexArrays( 1, &VAO );
+        glDeleteBuffers( 1, &VBO );
+        return 0;
       }
     }
 
     mainWindow.clear( sf::Color::Black );
+
+    glUseProgram( program );
+    glBindVertexArray( VAO );
+    glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+    glBindVertexArray( 0 );
+
     mainWindow.display();
   }
   return 0;
