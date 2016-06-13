f22fc0f1a7480b7bc5aca4c51a7d361f2ffd3927
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-13 19:47:31 -0400

    Simple transformations

---
+++ sfml_demo/3-glmtest/vertex.glsl
@@ -0,0 +1,20 @@
+#version 330 core
+layout (location = 0) in vec3 position; // The position variable has attribute position 0
+layout (location = 1) in vec3 color;	// The color variable has attribute position 1
+layout (location = 2) in vec2 texture;
+
+out vec3 fragColor; // Output a color to the fragment shader
+out vec2 fragTexture;
+
+uniform mat4 rotateAndScale;
+
+void pass() {
+  fragColor = color;
+  fragTexture = texture;
+}
+
+void main()
+{
+    gl_Position = rotateAndScale * vec4(position, 1.0f);
+    pass();
+}
