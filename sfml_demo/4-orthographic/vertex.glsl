07fb988122490d159012ae43aeffad94ad195f7e
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-16 16:49:43 -0400

    orthographic projection

---
+++ sfml_demo/4-orthographic/vertex.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+layout (location = 0) in vec3 position; // The position variable has attribute position 0
+layout (location = 1) in vec2 texture;
+
+out vec2 fragTexture;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void pass() {
+  fragTexture = texture;
+}
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(position, 1.0f);
+    pass();
+}
