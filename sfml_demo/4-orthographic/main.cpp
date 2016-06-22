bcee665e1670c06697dcc3b5434590233448e21a
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-22 19:52:05 -0400

    Loaded floor using assimp!

--- sfml_demo/4-orthographic/main.cpp
+++ sfml_demo/4-orthographic/main.cpp
@@ -37,83 +37,6 @@ int main() {
 
   glEnable( GL_DEPTH_TEST );
 
-  Model m( "./floor.obj" );
-
-  GLfloat vertices[] = {
-    -50.0f, 50.0f, 0.0f,     0.0f, 0.0f, 0.0f,  0.0f, 1.0f,
-    50.0f, 50.0f, 0.0f,      0.0f, 0.0f, 0.0f,  1.0f, 1.0f,
-    -50.0f, -50.0f, 0.0f,    0.0f, 0.0f, 0.0f,  0.0f, 0.0f,
-    -50.0f, -50.0f, 0.0f,    0.0f, 0.0f, 0.0f,  0.0f, 0.0f,
-    50.0f, 50.0f, 0.0f,      0.0f, 0.0f, 0.0f,  1.0f, 1.0f,
-    50.0f, -50.0f, 0.0f,     0.0f, 0.0f, 0.0f,  1.0f, 0.0f
-  };
-
-  GLuint VBO, VAO;
-  glGenVertexArrays( 1, &VAO );
-  glGenBuffers( 1, &VBO );
-
-  glBindVertexArray( VAO );
-
-    glBindBuffer( GL_ARRAY_BUFFER, VBO );
-      glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
-
-      glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0 );
-      glEnableVertexAttribArray( 0 );
-
-      glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)) );
-      glEnableVertexAttribArray( 1 );
-
-      glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat) ) );
-      glEnableVertexAttribArray( 2 );
-    glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-  glBindVertexArray( 0 );
-
-  GLuint alienTexture2;
-  {
-    sf::Image alien;
-    if( !alien.loadFromFile( "floor.png" ) ) {
-      std::cout << "Couldn't load required texture." << std::endl;
-      exit(1);
-    }
-
-    glGenTextures( 1, &alienTexture2 );
-    glBindTexture( GL_TEXTURE_2D, alienTexture2 );
-      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-
-      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-      auto size = alien.getSize();
-      glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, alien.getPixelsPtr() );
-      glGenerateMipmap( GL_TEXTURE_2D );
-    glBindTexture( GL_TEXTURE_2D, 0 );
-  }
-
-  sf::Clock clock;
-  sf::Font dosvga;
-  if( !dosvga.loadFromFile( "font.ttf" ) ) {
-    std::cout << "Couldn't load font.ttf" << std::endl;
-    return 1;
-  }
-
-  sf::Text text;
-  text.setFont( dosvga );
-  text.setCharacterSize( 16 );
-  text.setColor( sf::Color::White );
-
-  sf::Text coords;
-  coords.setFont( dosvga );
-  coords.setCharacterSize( 16 );
-  coords.setColor( sf::Color::Red );
-  coords.setPosition( 0, 16 );
-
-  sf::Text cameraCoords;
-  cameraCoords.setFont( dosvga );
-  cameraCoords.setCharacterSize( 16 );
-  cameraCoords.setColor( sf::Color::Cyan );
-  cameraCoords.setPosition( 0, 32 );
-
   // BlueBear is normally played through an isometric projection. Which isn't really true isometric, it's just the "90s isometric" look using an
   // orthographic projection and strategically placed camera. Our camera up vector is +Z, so objects on top of objects have increasing Z values.
   // THE IMPORTANT BIT: ** The camera goes -X and -Y for as much as the camera goes +Z from the Z of the point it looks at. **
@@ -126,11 +49,12 @@ int main() {
   // apply in that mode. Each mode begins with the same constaints as isometric mode, but some may or may not be kept:
   // Explorer View: All bets are off; you can go anywhere and do what you want. Compute the direction vector based on the pitch and yaw of the mouse.
   // Doll's-Eye View: This is explorer view, locked to the Z-height of the doll. The camera never goes higher or lower than the doll's eye height.
-
   sf::Vector2i center( ( SCREEN_WIDTH / 2 ), ( SCREEN_HEIGHT / 2 ) );
 
   LotCamera lotCamera( shader.Program, SCREEN_WIDTH, SCREEN_HEIGHT );
   mainWindow.setMouseCursorVisible( lotCamera.ortho );
+
+  Model m( "./floor.obj" );
 
   while( mainWindow.isOpen() ) {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -139,33 +63,16 @@ int main() {
     shader.use();
     lotCamera.position();
 
-    glActiveTexture( GL_TEXTURE0 );
-      glBindTexture( GL_TEXTURE_2D, alienTexture2 );
-      glUniform1i( glGetUniformLocation( shader.Program, "texture0" ), 0 );
+    for( int x = -16; x != 16; x++ ) {
+      for( int y = -16; y != 16; y++ ) {
+        glm::mat4 model;
+        model = glm::translate( model, glm::vec3( (GLfloat)(x * 100.0f), (GLfloat)(y * 100.0f), -1000.0f ) );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model ) );
 
-    GLuint uModel = glGetUniformLocation( shader.Program, "model" );
+        m.draw( shader );
+      }
+    }
 
-    glBindVertexArray( VAO );
-      // Floor tiles
-      //for( int x = -16; x != 16; x++ ) {
-        //for( int y = -16; y != 16; y++ ) {
-          glm::mat4 model;
-          model = glm::translate( model, glm::vec3( 0.0f, 0.0f, -1000.0f ) );
-          glUniformMatrix4fv( uModel, 1, GL_FALSE, glm::value_ptr( model ) );
-
-          glDrawArrays( GL_TRIANGLES, 0, 6 );
-        //}
-      //}
-    glBindVertexArray( 0 );
-
-    mainWindow.pushGLStates();
-      text.setString( lotCamera.ortho ? "Isometric" : "First-person" );
-      coords.setString( lotCamera.positionToString().c_str() );
-      cameraCoords.setString( lotCamera.directionToString().c_str() );
-      mainWindow.draw( text );
-      mainWindow.draw( cameraCoords );
-      mainWindow.draw( coords );
-    mainWindow.popGLStates();
 
     mainWindow.display();
 
@@ -179,8 +86,8 @@ int main() {
     while( mainWindow.pollEvent( event ) ) {
       if( event.type == sf::Event::Closed ) {
         mainWindow.close();
-        glDeleteVertexArrays( 1, &VAO );
-        glDeleteBuffers( 1, &VBO );
+        //glDeleteVertexArrays( 1, &VAO );
+        //glDeleteBuffers( 1, &VBO );
       }
 
       if( event.type == sf::Event::KeyPressed ) {
---
+++ sfml_demo/4-orthographic/model.hpp
@@ -0,0 +1,130 @@
+#ifndef MODEL3D
+#define MODEL3D
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <SFML/Graphics.hpp>
+#include <GL/glew.h>
+#include "mesh.hpp"
+
+class Model {
+
+  public:
+    Model( std::string path ) {
+      loadModel( path );
+    }
+    void draw( Shader shader ) {
+      for( Mesh& mesh : meshes ) {
+        mesh.draw( shader );
+      }
+    }
+
+  private:
+    std::vector< Mesh > meshes;
+    std::string directory;
+
+    void loadModel( std::string path ) {
+      Assimp::Importer importer;
+      const aiScene* scene = importer.ReadFile( path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals );
+
+      if( !scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode ) {
+        std::cout << "Warning: Could not import file " << path << std::endl;
+        return;
+      }
+
+      directory = path.substr( 0, path.find_last_of( '/' ) );
+
+      processNode( scene->mRootNode, scene );
+    }
+
+    void processNode( aiNode* node, const aiScene* scene ) {
+      for( int i = 0; i < node->mNumMeshes; i++ ) {
+        aiMesh* mesh = scene->mMeshes[ node->mMeshes[ i ] ];
+        this->meshes.push_back( this->processMesh( mesh, scene ) );
+      }
+
+      for( int i = 0; i < node->mNumChildren; i++ ) {
+        processNode( node->mChildren[ i ], scene );
+      }
+    }
+
+    Mesh processMesh( aiMesh* mesh, const aiScene* scene ) {
+      std::vector< Mesh::Vertex > vertices;
+      std::vector< Mesh::Index > indices;
+      std::vector< Mesh::Texture > textures;
+
+      for( int i = 0; i < mesh->mNumVertices; i++ ) {
+        Mesh::Vertex vertex;
+        vertex.position = glm::vec3( mesh->mVertices[ i ].x, mesh->mVertices[ i ].y, mesh->mVertices[ i ].z );
+        vertex.normal = glm::vec3( mesh->mNormals[ i ].x, mesh->mNormals[ i ].y, mesh->mNormals[ i ].z );
+        if( mesh->mTextureCoords[ 0 ] ) {
+          vertex.textureCoordinates = glm::vec2( mesh->mTextureCoords[ 0 ][ i ].x, mesh->mTextureCoords[ 0 ][ i ].y );
+        } else {
+          vertex.textureCoordinates = glm::vec2( 0.0f, 0.0f );
+        }
+        vertices.push_back( vertex );
+      }
+
+      for( int i = 0; i < mesh->mNumFaces; i++ ) {
+        aiFace face = mesh->mFaces[ i ];
+        for( int j = 0; j < face.mNumIndices; j++ ) {
+          indices.push_back( face.mIndices[ j ] );
+        }
+      }
+
+      if( mesh->mMaterialIndex >= 0 ) {
+        aiMaterial* material = scene->mMaterials[ mesh->mMaterialIndex ];
+        // Just do diffuse maps for now
+        textures = loadMaterialTextures( material, aiTextureType_DIFFUSE, "texture_diffuse" );
+      }
+
+      return Mesh( vertices, indices, textures );
+    }
+
+    std::vector< Mesh::Texture > loadMaterialTextures( aiMaterial* material, aiTextureType type, std::string typeName ) {
+      std::vector< Mesh::Texture > textures;
+
+      auto texCount = material->GetTextureCount( type );
+      for( int i = 0; i < texCount; i++ ) {
+        aiString str;
+        material->GetTexture( type, i, &str );
+        Mesh::Texture texture{ textureFromFile( str.C_Str(), directory ), typeName, str };
+        textures.push_back( texture );
+      }
+
+      return textures;
+    }
+
+    GLuint textureFromFile( std::string path, std::string directory ) {
+      sf::Image texture;
+      if( !texture.loadFromFile( directory + "/" + path ) ) {
+        std::cout << "Couldn't load texture " << path << std::endl;
+        return -1;
+      }
+
+      GLuint textureID;
+      glGenTextures( 1, &textureID );
+      glBindTexture( GL_TEXTURE_2D, textureID );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+        auto size = texture.getSize();
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.getPixelsPtr() );
+        glGenerateMipmap( GL_TEXTURE_2D );
+      glBindTexture( GL_TEXTURE_2D, 0 );
+
+      return textureID;
+    }
+};
+
+#endif
