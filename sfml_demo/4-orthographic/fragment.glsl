bef56ef7fce3bbfc8f481092a346b1b9a296bc25
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-06-30 12:46:34 -0400

    Cleanup, setTransformUniform for GraphicEntity

--- sfml_demo/4-orthographic/fragment.glsl
+++ sfml_demo/4-orthographic/fragment.glsl
@@ -5,8 +5,6 @@ out vec4 color;
 
 uniform sampler2D texture0;
 
-uniform bool isFloorTile;
-
 void main() {
   color = texture( texture0, fragTexture );
 }
--- sfml_demo/4-orthographic/graphicentity.hpp
+++ sfml_demo/4-orthographic/graphicentity.hpp
@@ -30,11 +30,39 @@ class GraphicEntity {
     glm::vec3 scale = glm::vec3( 1.0f, 1.0f, 1.0f );
     glm::vec3 rotationAxes = glm::vec3( 0.0f, 0.0f, 0.0f );
     GLfloat rotationAngle = 0.0f;
+    glm::mat4 transform;
     // Performance-related switches
     bool applyScale = false;
     bool applyRotation = false;
 
     GraphicEntity( const Model* model ) : model( model ) {}
+
+    /**
+     * Apply all transformation matricies to the object given translation, scale, and rotation
+     * glm::translate, glm::rotate, then glm::scale (the actual multiplication is done backwards).
+     *
+     * The call to position() sets the vertex shader uniform for the vertex position. Call it directly
+     * before a draw.
+     */
+    void setTransformUniform( GLuint shaderProgram ) {
+      transform = glm::mat4();
+
+      // There's always going to be a translation
+      transform = glm::translate( transform, position );
+
+      // Then rotate
+      if( applyRotation ) {
+        transform = glm::rotate( transform, rotationAngle, rotationAxes );
+      }
+
+      // Then scale
+      if( applyScale ) {
+        transform = glm::scale( transform, scale );
+      }
+
+      // Set the uniform for the shader
+      glUniformMatrix4fv( glGetUniformLocation( shaderProgram, "model" ), 1, GL_FALSE, glm::value_ptr( transform ) );
+    }
 };
 
 #endif
